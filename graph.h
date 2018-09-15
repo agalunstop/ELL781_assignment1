9e546886194336f188b78ea97c96094cf436d577
agalunstop <eez188143@ee.iitd.ac.in>
2018-09-15 13:52:34 +0530

    added RETRIEVE_EDGE; changed adj matrix type to bool

--- graph.h
+++ graph.h
@@ -3,7 +3,7 @@ struct graph		//adjacency matrix
 {
 	int matches[100];			// array to store name of matches
 	bool colored[100];		// save color status of nodes
-	int matrix[100][100];		// 2D array to store adjacency matrix
+	bool matrix[100][100];		// 2D array to store adjacency matrix
 }
 
 void MAKENULL(struct graph G);		
@@ -12,6 +12,7 @@ void CREATE_GRAPH(int n, struct graph G); // number of teams and struct
 //struct graph* NEXT(struct graph* G);
 int FIRST_UNCOLORED(struct graph G);	// position of first uncolored vertex
 int RETRIEVE_VERTEX(int p, struct graph G); // vertex at position p
+bool RETRIEVE_EDGE(int i, int j);	// return graph.matrix[i][j]
 
 int NEXT_UNCOLORED(int p, struct graph G);	// position of next uncolored vertex after p
 void MARK_COLORED(int p, struct graph G); // mark vertex at position p colored
