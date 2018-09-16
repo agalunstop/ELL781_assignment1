998ceb32e9da5f4be4dc842a177c01cdc5c33e80
Aditi Gupta <gupta89.aditi@gmail.com>
2018-09-16 17:29:10 +0530

    Added necessary comments

--- main.c
+++ main.c
@@ -10,23 +10,23 @@ void main(int argc, char *argv[])
 {
 	int n = atoi(argv[1]);
 	printf("Number of teams = %d\n",n);
-	if(n<2 || n>9)
+	if(n<2 || n>9)																			//Boundary cases for the program where it will not work.
 	{
 		printf("ERROR: Number of teams can be 2 to 9 only\n\n");
 		exit(1);
 	}
-	struct graph G;
-	struct graph *ptr_g = &G;
-	struct list new_clr;
-	struct list *ptr_new_clr = &new_clr;
+	struct graph G;					//Declaring Graph structure				
+	struct graph *ptr_g = &G;                       //Initializing pointer variable to graph
+	struct list new_clr;				//Declaring List structure
+	struct list *ptr_new_clr = &new_clr;		//Initaializing pointer variable to list
 
-	MAKENULL_G(n,ptr_g);
-	POPULATE(n,ptr_g);
+	MAKENULL_G(n,ptr_g);				//Creating an empty graph
+	POPULATE(n,ptr_g);				//populating Graph with team names
 
 	printf("matches: ");
 	for (int i=0; i<n*(n-1); i++)
 	{
-		printf("%d, ",G.matches[i]);
+		printf("%d, ",G.matches[i]);		//Printing all combination of matches between two teams.
 	}
 	printf("\n\n");
 
@@ -37,7 +37,7 @@ void main(int argc, char *argv[])
 	{
 		for (int j=0; j<n*(n-1);j++)
 		{
-	//		printf("%d ", G.matrix[i][j]);
+	//		printf("%d ", G.matrix[i][j]);	//DEBUG:printing the adjajcency matrix 
 		}
 	//	printf("\n");
 	}
@@ -48,9 +48,8 @@ void main(int argc, char *argv[])
 	int no_of_days = 0;
 	while(!ALL_COLORED(ptr_g))
 	{
-		MAKENULL_L(ptr_new_clr);
-		greedy(ptr_g, ptr_new_clr);
-
+		MAKENULL_L(ptr_new_clr);		//setting the new_clr list empty
+		greedy(ptr_g, ptr_new_clr);		//calling greedy recursively untill the whole graph is colored 
 		for(int i=0;i<END(ptr_new_clr);i++)
 		{
 			printf("(%d); ",ptr_new_clr->elements[i]);
